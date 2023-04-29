1bcda42f6c2b48e69fb9216d68d681ed7ba2301c
Zelejka <zelejkaplay@gmail.com>
2023-04-30 00:56:37 +0300

    4

--- test/cgi-bin/midlebox_script.py
+++ test/cgi-bin/midlebox_script.py
@@ -1,8 +1,33 @@
 import sqlite3
 
-connection = sqlite3.connect('cgi-bin/data1.db')
+img = 'img'
+
+connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
-cursor.execute('select * from goods;')
-x = cursor.fetchall()
+cursor.execute('SELECT COUNT(*) FROM stocks;')
+count = cursor.fetchall()
+count = str(count)
+res_cou = count.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
+res_cou = (int(res_cou))
+for j in range(res_cou):
+    cursor.execute(f'select name from stocks where id = {j+1};')
+    name = cursor.fetchone()
+    name = str(name)
+    name = name.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
+    cursor.execute(f'select comment from stocks where id = {j+1}')
+    comment = cursor.fetchone()
+    comment = str(comment)
+    comment = comment.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
+    cursor.execute(f'select price from stocks where id = {j+1}')
+    price = cursor.fetchone()
+    price = str(price)
+    price = price.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
+    print(f'''
+        <div class="projeckt{j+1}">
+            <img src="img/{img}" alt="">
+            <p><b>{name}</b></p>
+            <p>{comment}</p>
+            <p>{price}</p>
+        </div>
+    ''')
 connection.close()
-print(x)
