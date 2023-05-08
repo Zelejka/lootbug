8d8f2522181b2bc0a043781faf2696dd0c8eb289
Zelejka <96417882+Zelejka@users.noreply.github.com>
2023-05-08 20:56:03 +0300

    5

--- test/cgi-bin/midlebox_script.py
+++ test/cgi-bin/midlebox_script.py
@@ -1,33 +1,67 @@
 import sqlite3
+import json
+import urllib.parse
+import os
 
-img = 'img'
 
+# Получение параметров из HTTP-запроса
+query_string = os.environ.get("QUERY_STRING", "")
+query_dict = urllib.parse.parse_qs(query_string)
+_id = query_dict.get("id", [""])[0]
+
+
+html = ''
+
+# Установка заголовка Content-type для вывода HTML
+print('Content-type: text/html\n')
+
+# Создание подключения к базе данных SQLite и курсора
+# для выполнения SQL-запросов
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='stocks'")
+result = cursor.fetchone()
+
+if not result:
+    cursor.execute('''CREATE TABLE stocks
+             (id SERIAL PRIMARY KEY,
+              name text,
+               comment text,
+               img text,
+                price int)''')
+
+# Получение количества записей в таблице stocks
 cursor.execute('SELECT COUNT(*) FROM stocks;')
-count = cursor.fetchall()
-count = str(count)
-res_cou = count.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
-res_cou = (int(res_cou))
-for j in range(res_cou):
-    cursor.execute(f'select name from stocks where id = {j+1};')
-    name = cursor.fetchone()
-    name = str(name)
-    name = name.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
-    cursor.execute(f'select comment from stocks where id = {j+1}')
-    comment = cursor.fetchone()
-    comment = str(comment)
-    comment = comment.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
-    cursor.execute(f'select price from stocks where id = {j+1}')
-    price = cursor.fetchone()
-    price = str(price)
-    price = price.replace(",", "").replace("(", "").replace(")", "").replace("[", "").replace("]", "")
-    print(f'''
-        <div class="projeckt{j+1}">
-            <img src="img/{img}" alt="">
-            <p><b>{name}</b></p>
-            <p>{comment}</p>
-            <p>{price}</p>
+count = cursor.fetchone()[0]
+
+# Итерация по записям в таблице и формирование HTML-кода
+for j in range(count):
+    # Получение данных для текущей записи
+    cursor.execute('SELECT img, name, comment, price FROM stocks WHERE id = ?', (j+1,))
+    img, name, comment, price = cursor.fetchone()
+
+    # Преобразование данных в строки и замена кавычек для корректной вставки в HTML
+    img = str(img).replace("'", "")
+    name = str(name).replace("'", "")
+    comment = str(comment).replace("'", "")
+    price = str(price).replace("'", "")
+
+    # Формирование HTML-кода для текущей записи
+    html += (f'''
+        <div class="projeckt">
+            <img src="img/{img}.png" alt="" class='img' style="height: 100%; width: auto;">
+            <div class="textonprojeckt">
+                <p class='name'><b>{name}</b></p>
+                <p class='comment'>{comment}</p>
+                <p class='price'>{price}</p>
+                <p>sas id-{_id}</p>
+            </div>
         </div>
     ''')
+
+y = json.dumps(html)
+print(y)
+
+# Закрытие соединения с базой данных
 connection.close()
--- test/start.py
+++ test/start.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 server_adress = ("", 8000)
 httpd = HTTPServer(server_adress, CGIHTTPRequestHandler)
+print(True)
 httpd.serve_forever()
